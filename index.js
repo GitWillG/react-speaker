9964c8c9bf626b10927c9cd6e2702b249265c5b8
GitWillG <36644824+GitWillG@users.noreply.github.com>
2022-06-04 01:17:25 -0400

    Basic index for loading asset

--- index.js
+++ index.js
@@ -23,48 +23,103 @@ let beatFactorHi;
 let audio;
 let elapsedTime;
 
-export default (e) => {
+export default () => {
   const app = useApp()
   app.name = 'react-Speaker'
-  const cameraManager = useCameraManager();
-  // const gl = useInternals().renderer
-  // const physics = usePhysics()
-  // gl.outputEncoding = THREE.sRGBEncoding
+  const physics = usePhysics();
+  
+  // for screenshake later
+  //const cameraManager = useCameraManager();
+
+
+
+
+  // const audioTrackInformation = {
+  //   source: baseUrl + 'music/dungeon.mp3',
+  //   autoPlay: true,
+  // }
+
+  //seperate controls for testing
+  // //get music ->CLEAN
+  // document.body.onkeyup = (e) => {
+  //   if (e.code === 'Digit1') {
+  //     audio = getAudio({ createOnCall: false })
+  //     createAudio(audioTrackInformation)
+  //   }
+  // }
+  // //play  ->CLEAN
+  // document.body.onkeyup = (e) => {
+  //   if (e.code === 'Digit2') {
+  //       if (audio.paused) {
+  //         audio.play()
+  //       }
+  //     createAudio(audioTrackInformation)
+  //   }
+  // }
+  // //pause music ->CLEAN
+  // document.body.onkeyup = (e) => {
+  //   if (e.code === 'Digit3') {
+  //     if (audio.paused !== undefined) {
+  //         audio.pause()
+  //     }
+  //     createAudio(audioTrackInformation)
+  //   }
+  // }
+
+
+  //screen shake method for heavy bass
+  // const {animations} = o;
+  // o = o.scene;
+  // app.add(o);
+
+
+  //   localVector.setFromMatrixPosition(o.matrixWorld);
+  //   cameraManager.addShake(localVector, 0.2, 30, 500);
+  
+
+  // const _shake = () => {
+  //   _shake()
+  // }
  
-  };
-
-
-
-
-
-
-
-
-  //let physicsIds = [];  ->CLEAN
-  (async () => {
-    const u = `${baseUrl}react-Speaker.glb`;
-    let o = await new Promise((accept, reject) => {
-      const {gltfLoader} = useLoaders();
-      gltfLoader.load(u, accept, function onprogress() {}, reject);
-    });
-
-    //shake
-    // const {animations} = o;
-    // o = o.scene;
-    // app.add(o);
-
-
-    //   localVector.setFromMatrixPosition(o.matrixWorld);
-    //   cameraManager.addShake(localVector, 0.2, 30, 500);
-    
-
-    // const _shake = () => {
-    //   _shake()
-    // }
+  
+ // ->CLEAN
+ let physicsIds = [];
+ (async () => {
+  const u = `${baseUrl}react-Speaker.glb`;
+  let o = await new Promise((accept, reject) => {
+    const {gltfLoader} = useLoaders();
+    gltfLoader.load(u, accept, function onprogress() {}, reject);
   });
 
+  const physicsId = physics.addGeometry(o);
+  physicsIds.push(physicsId);
+  })();
+  
+  //cleanup
+  useCleanup(() => {
+    // composer.removePass(finalPass)
+    // composer.removePass(earthquakePass)
+    for (const physicsId of physicsIds) {
+      physics.removeGeometry(physicsId)
+    }
+  })
+  // return app
+  // };
+  return app;
+};
 
-  // alt loading method
+
+
+
+
+ 
+
+
+
+  
+
+
+// alt loading method
   // const speakerInfo = {
   //   fileName: 'speaker.glb',
   //   // filePath: baseUrl + './',
@@ -77,51 +132,3 @@ export default (e) => {
   // //     app.add(model)
   // //   })
   // // })
-  // const audioTrackInformation = {
-  //   source: baseUrl + 'tracks/music.wav',
-  //   autoPlay: true,
-  //   // currentTime: 100.2,
-  // }
-
-  //get music ->CLEAN
-  document.body.onkeyup = (e) => {
-    if (e.code === 'Digit1') {
-      audio = getAudio({ createOnCall: false })
-      createAudio(audioTrackInformation)
-    }
-  }
-  
-  //play  ->CLEAN
-  document.body.onkeyup = (e) => {
-    if (e.code === 'Digit2') {
-      
-        if (audio.paused) {
-          audio.play()
-        }
-      
-      createAudio(audioTrackInformation)
-    }
-  }
-  //pause music ->CLEAN
-  document.body.onkeyup = (e) => {
-    if (e.code === 'Digit3') {
-      if (audio.paused !== undefined) {
-        
-          audio.pause()
-        
-      }
-      createAudio(audioTrackInformation)
-    }
-  }
-
-
-//cleanup
-  useCleanup(() => {
-    // composer.removePass(finalPass)
-    // composer.removePass(earthquakePass)
-    for (const physicsId of physicsIds) {
-      physics.removeGeometry(physicsId)
-    }
-  })
-  return app
-};
