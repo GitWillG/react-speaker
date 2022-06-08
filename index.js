0742a1e14f9bb8cb8ba32da2cf74e48426d29404
GitWillG <36644824+GitWillG@users.noreply.github.com>
2022-06-08 15:23:32 -0400

    different load method

--- index.js
+++ index.js
@@ -26,37 +26,101 @@ let mid;
 let morphTargets =[];
 
 export default () => {
-  const app = useApp();
-  app.name = 'react-Speaker';
-  const physics = usePhysics();
+//   const app = useApp();
+//   app.name = 'react-Speaker';
+//   const physics = usePhysics();
   
  
   
- // ->CLEAN
- let physicsIds = [];
- (async () => {
-  const u = `${baseUrl}react-speaker.glb`;
-  console.log(u);
-  let o = await new Promise((accept, reject) => {
-    const {gltfLoader} = useLoaders();
-    gltfLoader.load(u, accept, function onprogress() {}, reject);
-  });
-  console.log("glb data", o.morphTargets, o);
-  const physicsId = physics.addGeometry(o);
-  physicsIds.push(physicsId);
+//  // ->CLEAN
+//  let physicsIds = [];
+//  (async () => {
+//   const u = `${baseUrl}react-speaker.glb`;
+//   console.log(u);
+//   let o = await new Promise((accept, reject) => {
+//     const {gltfLoader} = useLoaders();
+//     gltfLoader.load(u, accept, function onprogress() {}, reject);
+//   });
+//   console.log("glb data", o.morphTargets, o);
+//   const physicsId = physics.addGeometry(o);
+//   physicsIds.push(physicsId);
+//   })();
+  
+  
+//   //cleanup
+//   useCleanup(() => {
+//     // composer.removePass(finalPass)
+//     // composer.removePass(earthquakePass)
+//     for (const physicsId of physicsIds) {
+//       physics.removeGeometry(physicsId);
+//     }
+//   });
+//   // return app
+//   // };
+
+  const app = useApp();
+  let fan = null;
+  //let prop = null;
+  const physics = usePhysics();
+  const physicsIds = [];
+  (async () => {
+      const u = `${baseUrl}/react-speaker.glb`;
+      speaker = await new Promise((accept, reject) => {
+          const {gltfLoader} = useLoaders();
+          gltfLoader.load(u, accept, function onprogress() {}, reject);
+          
+      });
+      // fan.scene.traverse(o => {
+      //   // if (o.isMesh) {
+      //   //   console.log(o);
+      //   // }
+      //   if(o.name === 'Plane001') {  prop = o; }
+      // });
+      //fan.scene.position.y=1;
+      speaker.scene.scale.set(1,1,1);
+      //fan.scene.rotation.x = Math.PI/2;
+      // group.add(fan.scene);
+      app.add(speaker.scene);
+      let physicsId;
+      physicsId = physics.addGeometry(speaker.scene);
+      physicsIds.push(physicsId)
+      // const geometry = new THREE.CircleGeometry( 1, 32 );
+      // const material = new THREE.MeshBasicMaterial( { color: 0xff0000, transparent:true, opacity:0.5, side: THREE.DoubleSide } );
+      // const circle = new THREE.Mesh( geometry, material );
+      // circle.rotation.x = Math.PI / 2;
+      // circle.position.y = 0.1;
+      // app.add( circle );
+      app.updateMatrixWorld();
+
+
   })();
+
   
   
-  //cleanup
+
+  // useFrame(( { timeStamp } ) => {
+  //   if(prop){
+  //     prop.rotation.x = -1.570799097288404; 
+  //     prop.rotation.y += -1.4884504324181542; 
+  //     prop.rotation.z = -3.141592653589793; 
+  //   }
+  //   app.updateMatrixWorld();
+  // });
+
+  
   useCleanup(() => {
-    // composer.removePass(finalPass)
-    // composer.removePass(earthquakePass)
     for (const physicsId of physicsIds) {
       physics.removeGeometry(physicsId);
     }
   });
-  // return app
-  // };
+
+
+
+
+
+
+
+
   return app; 
 };
 
