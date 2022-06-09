19fb240c0e9a1b623b01f42bfd2d16994f0fda7a
GitWillG <36644824+GitWillG@users.noreply.github.com>
2022-06-09 02:42:34 -0400

    declaring speaker as object

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ export default () => {
 
   // declare the app and speaker, as well as physics
   const app = useApp();
-  let speaker = null;
+  let speaker = new THREE.Object3D();
   //let prop = null;
   const physics = usePhysics();
   const physicsIds = [];
