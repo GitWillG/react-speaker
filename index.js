4f77378417ac5ab18bfa746e5832eefe830f2cf2
GitWillG <36644824+GitWillG@users.noreply.github.com>
2022-06-13 13:32:50 -0400

    alter position after adding to scene

--- index.js
+++ index.js
@@ -62,8 +62,8 @@ export default () => {
       // scale and insert into scene
       speaker.scene.scale.set(4,4,4);
       speaker.scene.rotation.y = Math.PI;
-      speaker.scene.position = new THREE.Vector3(94, 5, 43);
       app.add(speaker.scene);
+      speaker.scene.position = new THREE.Vector3(94, 5, 43);
       let physicsId;
       physicsId = physics.addGeometry(speaker.scene);
       physicsIds.push(physicsId);
